7fc76b0608e60b0b1fec57638c140e798d89b0cc
edloidas <edloidas@gmail.com>
2016-12-06 00:14:20 +0300

    :bug: Fix files paths resolve
    Fix the running context root path, that was `node_modules/bin`

--- index.js
+++ index.js
@@ -6,11 +6,19 @@ First argument without the option is the module name
 Other argumets are the parameters, that will be passed
 */
 var argv = require( 'minimist' )( process.argv.slice( 2 ));
+var path = require( 'path' );
 
 module.exports = function runwith() {
   if ( argv._.length > 0 ) {
     var module = argv._[ 0 ];
     var args = argv._.slice( 1 );
+
+    // local module, not from `node_modules`
+    // path will be resolved to the root of project it is used from
+    if ( path.parse( module ).name !== module ) {
+      module = path.resolve( module );
+    }
+
     return require( module ).apply( null, args );
   }
   return null;
--- test/index.test.js
+++ test/index.test.js
@@ -1,9 +1,15 @@
 const execFileSync = require( 'child_process' ).execFileSync;
 
-it( 'should not find module "unknown-module"', () => {
+it( 'should not find global module "unknown-module"', () => {
+  const result = execFileSync( 'node', [ './test/runwith.js', 'unknown-module', '1', '2' ]);
+  const msg = result.toString( 'utf8' ).trim();
+  expect( msg.includes( "Cannot find module 'unknown-module'" )).toEqual( true );
+});
+
+it( 'should not find local module "unknown-module"', () => {
   const result = execFileSync( 'node', [ './test/runwith.js', './test/unknown-module', '1', '2' ]);
   const msg = result.toString( 'utf8' ).trim();
-  expect( msg.includes( "Cannot find module './test/unknown-module'" )).toEqual( true );
+  expect( msg.includes( 'Cannot find module' )).toEqual( true );
 });
 
 it( 'should run local module "local-module"', () => {
